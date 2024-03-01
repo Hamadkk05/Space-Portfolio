ef2f38f959c96bcf91d364a1aeadb2e42ca6461e
Hamadkk05 <140351542+Hamadkk05@users.noreply.github.com>
2024-03-01 19:35:56 +0500

    Adding some sections

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             <a href="#about-me" className='h-auto w-auto flex flex-row items-center'>
                 <img src="/NavLogo.png" alt="logo" width={70} height={70} className='cursor-pointer hover:animate-slowspin' />
                 <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
-                    WebChain Dev
+                    Hamad's Portfolio
                 </span>
             </a>
             <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
