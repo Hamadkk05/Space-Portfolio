de0f9ae565175d221442978909fd0fa9db46d373
Hamadkk05 <140351542+Hamadkk05@users.noreply.github.com>
2024-04-20 01:49:39 +0500

    Adding some Stuff

--- components/main/skills.tsx
+++ components/main/skills.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Frontend_skill } from "@/constants";
+import { Backend_skill, Frontend_skill, Skill_data } from "@/constants";
 import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
@@ -8,13 +8,36 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center justify-center gap-3 h-full overflow-hidden py-20"
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80  py-20"
       style={{ transform: "scale(0.9)" }}
     >
       <SkillText/>
+
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Skill_data.map((image, index) => (
+          <SkillDataProvider
+          key={index}
+          src={image.Image}
+          width={image.width}
+          height={image.height}
+          index={index}
+          />
+        ))}
+      </div>
     
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Frontend_skill.map((image, index) => (
+          <SkillDataProvider
+          key={index}
+          src={image.Image}
+          width={image.width}
+          height={image.height}
+          index={index}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Backend_skill.map((image, index) => (
           <SkillDataProvider
           key={index}
           src={image.Image}
