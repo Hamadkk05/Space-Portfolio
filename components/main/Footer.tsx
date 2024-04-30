e57e9d0c8b70a9e1301082531e127cc0d36af10d
Hamadkk05 <140351542+Hamadkk05@users.noreply.github.com>
2024-05-01 02:08:04 +0500

    adding Cards section

---
+++ components/main/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { RxDiscordLogo,
+    RxGithubLogo,
+    RxInstagramLogo,
+    RxTwitterLogo,
+    RxLinkedinLogo,
+ } from "react-icons/rx";
+ 
+
+const Footer = () => {
+  return (
+    <div className=' w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] '>
+        <div className=' w-full flex flex-col items-center justify-center m-auto'>
+            <div className=' w-full h-full flex flex-row items-center justify-around flex-wrap'>
+                <div className='min-w-[200px] h-auto flex flex-col items-center justify-start'>
+                    <div className='font-bold text-[16px]'>
+                        Community
+
+                    </div>
+                    <p className='flex flex-row items-center my-[15px] cursor-pointer'>
+                       <RxGithubLogo/>
+                       <span className='text-[15px]'> Github</span>
+                    </p>
+                    
+                </div>
+
+            </div>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
